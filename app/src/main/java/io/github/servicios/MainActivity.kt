580a694d3396a1b5a4b20e8abc7f853d3189621b
utn203241069 <203241069@alumnos.utn.edu.mx>
2021-11-12 22:28:28 -0600

    Aplicacion en desarrollo
    Solo muestra lo esta en la pagina

--- app/src/main/java/io/github/servicios/MainActivity.kt
+++ app/src/main/java/io/github/servicios/MainActivity.kt
@@ -1,11 +1,46 @@
 package io.github.servicios
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mensaje =
+            findViewById<TextView>(R.id.mensaje)
+        Thread {
+            val connection =
+                URL("https://servicios.utn203241069.repl.co/")
+                    .openConnection()
+                        as HttpURLConnection
+            connection.connect()
+            val code =
+                connection.responseCode
+            if (200 <= code && code < 300) {
+                val text =
+                    connection.inputStream.use {
+                        it.reader()
+                            .use { reader -> reader.readText() }
+                    }
+                mensaje.post {
+                    mensaje.text = text
+                }
+            } else {
+                mensaje.post {
+                    mensaje.text =
+                        connection.responseMessage
+                }
+            }
+            connection.disconnect()
+        }.start()
+      }
     }
-}
+
+
+
